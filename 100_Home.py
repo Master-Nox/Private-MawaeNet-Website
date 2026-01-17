f2778e68e92e75269331f928a666e99ee0d7228b
Kyle Mawae <kamikazekeale@hotmail.com>
2026-01-16 23:07:22 -0600

    Updated files:  100_Home.py

--- 100_Home.py
+++ 100_Home.py
@@ -1,5 +1,42 @@
 import streamlit as st
 import base64
+from streamlit_extras.bottom_container import bottom
+from streamlit_extras.row import row
+
+st.set_page_config(
+    page_title="MawaeNet",
+    page_icon="🌐",
+    layout="wide"
+)
+
+st.markdown(
+    """
+        <style>
+                .stAppHeader {
+                    background-color: rgba(255, 255, 255, 0.0);  /* Transparent background */
+                    visibility: visible;  /* Ensure the header is visible */
+                }
+
+               .block-container {
+                    padding-top: 2rem;
+                    padding-bottom: 0rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+        </style>
+        """,
+    unsafe_allow_html=True,
+)
+
+# st.logo(image=r"MawaeNet.png", size="large", link="https://mawae.net")
+
+toprow = row(6, vertical_align="center", gap="medium")
+toprow.markdown("")
+toprow.markdown("")
+toprow.markdown("")
+toprow.markdown("")
+toprow.markdown("")
+toprow.link_button("Home", type="secondary", use_container_width=True, url="https://mawae.net", help="Go to the Home page.") # When moved over to its own url, this will be substituted in.
 
 # Load images as base64 for embedding in HTML
 try:
@@ -34,7 +71,6 @@ except FileNotFoundError:
 
 st.markdown("<h1 style='text-align: center;'>MawaeNet</h1>", unsafe_allow_html=True)
 st.markdown("<h5 style='text-align: center;'>Welcome to the Private MawaeNet.</h5>", unsafe_allow_html=True)
-st.markdown("<h1 style='text-align: center;'>This needs to be moved to its own streamlit file before going live!</h1>", unsafe_allow_html=True)
 
 # Create three columns for the link boxes
 col1, col2, col3 = st.columns(3)
@@ -98,3 +134,13 @@ with col3:
 </div>
 </a>
 ''', unsafe_allow_html=True)
+    
+with bottom():
+    st.markdown("---")
+    bottomrow = row(6, vertical_align="center", gap="medium")
+    bottomrow.markdown("For questions, please reach out on Discord @masternox")
+    bottomrow.markdown("")
+    bottomrow.markdown("")
+    bottomrow.markdown("")
+    bottomrow.markdown("")
+    bottomrow.link_button("Forgot Password?", type="secondary", use_container_width=True, url="https://account.mawae.net/reset-password/step1", help="Reset your password.")
